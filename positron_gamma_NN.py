c04bc2790394d48ddf4c263a3944908233fd0d48
macostav <mikelacvz@gmail.com>
2025-08-22 14:32:01 -0700

    First commit.

---
+++ positron_gamma_NN.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import sys
+from tqdm import tqdm
+
+"""
+Simple CNN to detect straight lines vs parabolas.
+"""
+
+class CaloDataset(Dataset):
+    def __init__(self, data_dir, transform = None):
+        pass # TODO: Implement dataset loading logic here
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
+class CaloClassifier(nn.Module):
+    def __init__(self, num_classes = 2):
+        super(CaloClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True) # The weights are already set; no need to train them
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1]) # Get rid of final layer of model
+
+        enet_output_size = 1280 # The output size of the model
+        
+        # Make a classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(), # Flatten the tensors into a 1D vector
+            nn.Linear(enet_output_size, num_classes) # Maps the 1280 output to our two classes; line or parabola
+        )
+
+    def forward(self, x):
+        # Connect these parts and return the output
+        x = self.features(x) # Pattern recognition part
+        output = self.classifier(x) # Last layer for classification of images
+        return output
+
+if __name__ == "__main__":
+    # Setting up the data
+    transform = transforms.ToTensor() # Convert images to tensors
+
+    train_dataset = CaloDataset(data_dir = "data/training", transform = transform) # label: 0 is parabola; 1 is straight line
+    test_dataset = CaloDataset(data_dir = "data/testing", transform = transform)
+
+    train_loader = DataLoader(train_dataset, batch_size = 32, shuffle = True)
+    test_loader = DataLoader(test_dataset, batch_size =32, shuffle = False) # Don't need shuffling when checking model accuracy
+
+    # Training loop
+    num_epochs = 50
+    train_losses, test_losses = [], []
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # Use GPU if possible for faster training
+
+    model = CaloClassifier(num_classes = 2)
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss() # Loss function; standard for multi-class classification; penalizes high confidence wrong predictions
+    optimizer = optim.Adam(model.parameters(), lr=0.001) # Algorithm to move to minimum of loss function; Adam is considered to be one of the best algorithms
+
+    best_accuracy = 0.0
+    accuracies = []
+    for epoch in range(num_epochs):
+        # Training loop
+        model.train()
+        running_loss = 0.0
+        for images, labels in tqdm(train_loader, desc='Training loop'):
+            # Move inputs and labels to the device
+            images, labels = images.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * labels.size(0)
+        train_loss = running_loss / len(train_loader.dataset)
+        train_losses.append(train_loss)
+
+        # Validation loop
+        model.eval()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad(): 
+            for images, labels in tqdm(test_loader, desc='Validation loop'):
+                # Move inputs and labels to the device
+                images, labels = images.to(device), labels.to(device)
+            
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item() * labels.size(0)
+
+                # Get prediction
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        test_loss = running_loss / len(test_loader.dataset)
+        accuracy = correct / total * 100
+        test_losses.append(test_loss)
+        accuracies.append(accuracy)
+
+        print(f"Epoch {epoch+1}/{num_epochs} - "
+        f"Train loss: {train_loss:.4f}, "
+        f"Validation loss: {test_loss:.4f}, "
+        f"Accuracy: {accuracy:.2f}%")
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save(model.state_dict(), "pretrained.pth")
+
+    plt.plot(train_losses, label='Training loss')
+    plt.plot(test_losses, label='Test loss')
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("Loss over Epochs - Pretrained")
+    plt.savefig("pretrained_loss_evolution.png")
+
+    plt.figure()
+    plt.plot(accuracies)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.title("Accuracy over Epochs - Pretrained")
+    plt.savefig("pretrained_accuracy_evolution.png")
+
